91ae08d7bb53c0637c345941a81b934ed37fbbbd
stephanminges <stephanminges@gmail.com>
2022-07-24 16:37:33 -0500

    first commit

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -1,16 +1,12 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 
-export class VpcStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class VpcStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ec2.Vpc(this, "MyVPC", {
+      cidr: "10.0.0.0/16",
+    });
   }
 }
